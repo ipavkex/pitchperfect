aa2e8e77d1bea25a17e900e9bdffc8203d6d6317
Ivan Pavlov <ipavkex@gmail.com>
2015-09-01 16:18:06 +1200

    Second commit, all tasks plus the improvement tasks implemented. Legacy code removed.

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -67,9 +67,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
         if flag{
             recordedAudio = RecordedAudio(filePathURL: recorder.url, title: recorder.url.lastPathComponent)
-            
-            //recordedAudio.filePathURL = recorder.url
-            //recordedAudio.title = recorder.url.lastPathComponent
         
             self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
         } else {
